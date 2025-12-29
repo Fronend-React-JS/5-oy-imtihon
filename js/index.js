b77a946db247bbb98c4d5601f94ad14b03565e35
Fronend-React-JS <144875501+Fronend-React-JS@users.noreply.github.com>
2025-12-29 16:23:43 +0500

    yangilandi bu

--- js/index.js
+++ js/index.js
@@ -1,111 +1,106 @@
+// DOM elementlarini olish
 const movieSearchBox = document.getElementById("search");
 const cards = document.querySelector(".cards");
 const searchBtn = document.getElementById("search_btn");
-const inputSearchblock = document.getElementById("input_s_b");
 
+// Kino qidirish tugmasi bosilganda
 searchBtn && searchBtn.addEventListener("click", () => {
-    const searchTerm = movieSearchBox.value;
+    const searchTerm = movieSearchBox.value.trim();
 
-    if (searchTerm.trim() !== "") {
+    if (searchTerm !== "") {
         fetchMovies(searchTerm);
+    } else {
+        alert("Iltimos, kino nomini kiriting!");
     }
 });
 
+// Kino qidirish funksiyasi
 function fetchMovies(searchTerm) {
-    
-    fetch(
-        `https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=${searchTerm}`
-    )
-        .then((data) => data.json())
-        .then((dataJson) => {
-
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query=${searchTerm}`)
+        .then(data => data.json())
+        .then(dataJson => {
             let lists = '';
 
             if (dataJson.results.length === 0) {
-                alert(`"${searchTerm}" degan kino topilmadi `);
+                alert(`"${searchTerm}" degan kino topilmadi!`);
                 return;
             }
 
-
-            dataJson.results.forEach((movie) => {
+            dataJson.results.forEach(movie => {
                 if (movie.vote_average >= 7) {
-                    let card = `
+                    lists += `
                         <div class="card">
                             <div class="movie-poster">
-                                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="movie poster">
+                                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">
                             </div>
                             <div class="movie-info">
                                 <h3 class="movie-title">${movie.original_title}</h3>
                                 <ul class="movie-misc-info">
-                                    <li class="year">Year: ${movie.release_date}</li>
-                                    <li class="rated">Vote_average: ${movie.vote_average}</li>
+                                    <li class="year">Yil: ${movie.release_date}</li>
+                                    <li class="rated">Bahosi: ${movie.vote_average}</li>
                                 </ul>
-                                <p class="language"><b>Language:</b> ${movie.original_language}</p>
-                                <p class="overview"><b>Overview:</b> ${movie.overview}</p>
-                                <span class="bookmark"><i class="fa-regular fa-bookmark"></i></span>
+                                <p class="language"><b>Tili:</b> ${movie.original_language}</p>
+                                <p class="overview"><b>Ta’rif:</b> ${movie.overview}</p>
+                                <span class="bookmark" data-movie-id="${movie.id}"><i class="fa-regular fa-bookmark"></i></span>
                             </div>
                         </div>
                     `;
-
-                    lists += card;
                 }
             });
 
-
-            cards.innerHTML = lists; 
+            cards.innerHTML = lists;
+            attachBookmarkEvents(dataJson.results);
         })
-        .catch((error) => {
-            console.log(error);
-        });
+        .catch(err => console.error("Xatolik yuz berdi:", err));
 }
 
-fetch(
-    `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=7`
-)
-    .then((data) => data.json())
-    .then((dataJson) => {
-        
+// Bookmark tugmalariga event qo‘shish
+function attachBookmarkEvents(movies) {
+    const bookmarks = document.querySelectorAll('.bookmark');
+    bookmarks.forEach(bookmark => {
+        bookmark.addEventListener('click', function () {
+            const movieId = this.dataset.movieId;
+            const selectedMovie = movies.find(movie => movie.id == movieId);
+
+            let savedMovies = localStorage.getItem('savedMovies') ? JSON.parse(localStorage.getItem('savedMovies')) : [];
+            if (!savedMovies.some(m => m.id === selectedMovie.id)) {
+                savedMovies.push(selectedMovie);
+                localStorage.setItem('savedMovies', JSON.stringify(savedMovies));
+                alert(`${selectedMovie.original_title} saqlandi!`);
+            } else {
+                alert(`${selectedMovie.original_title} allaqachon saqlangan!`);
+            }
+        });
+    });
+}
+
+// Dastlabki mashhur kinolarni chiqarish
+fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1`)
+    .then(data => data.json())
+    .then(dataJson => {
         let lists = '';
-        const cards = document.querySelector(".cards");         
-        dataJson.results.forEach((movie) => {
+        dataJson.results.forEach(movie => {
             if (movie.vote_average >= 7) {
-                let card = `
+                lists += `
                     <div class="card">
                         <div class="movie-poster">
-                            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="movie poster">
+                            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title}">
                         </div>
                         <div class="movie-info">
                             <h3 class="movie-title">${movie.original_title}</h3>
                             <ul class="movie-misc-info">
-                                <li class="year">Year: ${movie.release_date}</li>
-                                <li class="rated">Vote_average: ${movie.vote_average}</li>
+                                <li class="year">Yil: ${movie.release_date}</li>
+                                <li class="rated">Bahosi: ${movie.vote_average}</li>
                             </ul>
-                            <p class="language"><b>Language:</b> ${movie.original_language}</p>
-                            <p class="overview"><b>Overview:</b> ${movie.overview}</p>
+                            <p class="language"><b>Tili:</b> ${movie.original_language}</p>
+                            <p class="overview"><b>Ta’rif:</b> ${movie.overview}</p>
                             <span class="bookmark" data-movie-id="${movie.id}"><i class="fa-regular fa-bookmark"></i></span>
                         </div>
                     </div>
                 `;
-        
-                lists += card;
             }
         });
-        
-        cards.innerHTML += lists;
-        
-        const bookmarks = document.querySelectorAll('.bookmark');
-        
-        bookmarks.forEach((bookmark) => {
-            bookmark.addEventListener('click', function () {
-                const movieId = this.dataset.movieId;
-        
-                const selectedMovie = dataJson.results.find((movie) => movie.id == movieId);
-        
-                localStorage.setItem('selectedMovie', JSON.stringify(selectedMovie));
-            });
-        });
-        
-    })
-    .catch((error) => {
-        console.log(error);
+
+        cards.innerHTML = lists;
+        attachBookmarkEvents(dataJson.results);
     });
